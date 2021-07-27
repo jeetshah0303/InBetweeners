22a43a38d23e3ffbd62206ddcfe3330f88c5f291
Jeet Shah <55347539+jeetshah0303@users.noreply.github.com>
2021-07-28 00:10:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 import cv2
 app = Flask(__name__)
 @app.route('/')
-def dataEnrichment():
+def Main():
 
     return 'Hello World'
 
