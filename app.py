a829a0fcac05b95e2a0fcd5c3417b9f3602179a3
Jeet Shah <55347539+jeetshah0303@users.noreply.github.com>
2021-07-28 00:09:47 +0530

    Flask Outline
    flask outline to start work with simple web app

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session, make_response
+import cv2
+app = Flask(__name__)
+@app.route('/')
+def dataEnrichment():
+
+    return 'Hello World'
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.9',port=4455,debug=True)
