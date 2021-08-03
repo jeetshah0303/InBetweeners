6a1de2e4fa05961e6869a100c4a940e19b9d80d9
Jeet Shah <55347539+jeetshah0303@users.noreply.github.com>
2021-08-03 23:01:42 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session, make_response
 import cv2
 app = Flask(__name__)
-@app.route('/')
-def Main():
-
-    return 'Hello World'
-
+@app.route('/', methods=["GET", "POST"])
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(host='127.0.0.9',port=4455,debug=True)
